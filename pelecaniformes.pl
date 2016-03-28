ae251aa3d388166cbdba90573c6e32a05d0b8a9f
Cory Hutchison <cory.hutchison@protonmail.ch>
2016-03-28 12:20:24 -0600

    Initial commit

---
+++ pelecaniformes.pl
@@ -0,0 +1,38 @@
+order(?A).
+
+family(?A).
+
+genus(?A).
+
+species(?A).
+
+hasParent(?A, ?B).
+
+hasCommonName(?N, ?C).
+
+hasCommonName(?G, ?S, ?C).
+
+hasSciName(?C, ?N).
+
+hasCompoundName(?G, ?S, ?N).
+
+isaStrict(?A, ?B).
+
+isa(?A, ?B).
+
+synonym(?A, ?B).
+
+countSpecies(?A, -N).
+
+rangesTo(?A, ?P).
+
+habitat(?A, ?B).
+
+food(?A, ?B).
+
+nesting(?A, ?B).
+
+behavior(?A, ?B).
+
+conservation(?A, ?B).
+

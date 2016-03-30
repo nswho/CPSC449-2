6096989169bb207411f3be708d5490ccbac909dd
hthkwan <herman_fire@hotmail.com>
2016-03-30 00:37:30 -0600

    Basic rules established

--- pelecaniformes.pl
+++ pelecaniformes.pl
@@ -1,14 +1,83 @@
-order(?A).
+%groups
+order(pelecaniformes).
+family(pelecanidae).
+family(ardeidae).
+family(threskiornithdae).
+genus(pelecanus).
+genus(botaurus).
+genus(ixobrychus).
+genus(ardea).
+genus(egretta).
+genus(bubulcus).
+genus(butorides).
+genus(nycticorax).
+genus(nyctanassa).
+genus(eudocimus).
+genus(plegadis).
+genus(platalea).
+species(erythrorhynchos).
+species(occidentalis).
+species(lentiginosus).
+species(exilis).
+species(herodias).
+species(alba).
+species(thulaegretta).
+species(caerulea).
+species(tricolor).
+species(rufescens).
+species(ibis).
+species(virescens).
+species(nycticorax).
+species(violacea).
+species(albus).
+species(falcinellus).
+species(chihi).
+species(ajaja).
 
-family(?A).
+% common names
+pelecanus(pelican).
+erythrorhynchos(americanWhitePelican).
+occidentalis(brownPelican).
+botaurus(bittern).
+lentiginosus(americanBittern).
+ixobrychus(bitern).
+exilis(leastBittern).
+ardea(heron).
+herodias(greatBlueHeron).
+alba(greatEgret).
+egretta(heron, egret). /*maybe split these 2 up?*/
+thulaegretta(snowyEgret).
+caerulea(littleBlueHeron).
+tricolor(tricoloredHeron).
+rufescens(reddishEgret).
+bubulcus(egret).
+ibis(cattleEgret).
+butorides(heron).
+virescens(greenHeron).
+nycticorax(nightHeron).
+nycticorax(blackCrownedNightHeron).
+nyctanassa(nightHeron).
+violacea(yellowCrownedNightHeron).
+eudocimus(ibis).
+albus(whiteIbis).
+plegadis(ibis).
+falcinellus(glossyIbis).
+chihi(whiteFacedIbis).
+platalea(spoonbill).
+ajaja(roseateSpoonbill).
 
-genus(?A).
 
-species(?A).
+order(A).
 
-hasParent(?A, ?B).
+family(A).
 
-hasCommonName(?N, ?C).
+genus(A).
+
+species(A).
+
+hasParent(A, B).
+
+hasCommonName(N, C).
 
 hasCommonName(?G, ?S, ?C).
 

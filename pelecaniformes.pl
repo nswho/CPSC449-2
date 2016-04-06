28d4707e5b03994e5dafd77a0e7a0986ce861e3f
Cory Hutchison <cory.hutchison@protonmail.ch>
2016-04-06 16:19:35 -0600

    Finished more cases, and have 74/214 tests passing

--- pelecaniformes.pl
+++ pelecaniformes.pl
@@ -67,8 +67,28 @@ commonName(chihi, whiteFacedIbis).
 commonName(platalea, spoonbill).
 commonName(ajaja, roseateSpoonbill).
 
+%compound names
+compoundName(pelecanus, erythrorhynchos, pelecanus_erythrorhynchos).
+compoundName(pelecanus, occidentalis, pelecanus_occidentalis).
+compoundName(botaurus, lentiginosus, botaurus_lentiginosus).
+compoundName(ixobrychus, exilis, ixobrychus_exilis).
+compoundName(ardea, herodias, ardea_herodias).
+compoundName(ardea, alba, ardea_alba).
+compoundName(egretta, thula, egretta_thula).
+compoundName(egretta, caerulea, egretta_caerulea).
+compoundName(egretta, tricolor, egretta_tricolor).
+compoundName(egretta, rufescens, egretta_rufescens).
+compoundName(bubulcus, ibis, bubulcus_ibis).
+compoundName(butorides, virescens, butorides_virescens).
+compoundName(nycticorax, nycticorax, nycticorax_nycticorax).
+compoundName(nyctanassa, violacea, nyctanassa_violacea).
+compoundName(eudocimus, albus, eudocimus_albus).
+compoundName(plegadis, falcinellus, plegadis_falcinellus).
+compoundName(plegadis, chihi, plegadis_chihi).
+compoundName(platalea, ajaja, platalea_ajaja).
+
 %definitions
-%order(_) :- pelecaniformes. 
+%order(A). 
 
 %family(A).
 
@@ -76,43 +96,40 @@ commonName(ajaja, roseateSpoonbill).
 
 %species(A).
 
-hasParent(A,B) :-
-    order(B),
-    family(A);
-    order(B),
-    genus(A);
-    order(B),
-    species(A);
-    family(B),
-    genus(A);
-    family(B),
-    species(A);
-    genus(B),
-    species(A).
+hasParent(A, B) :-
+    order(B), family(A);
+    order(B), genus(A);
+    order(B), species(A);
+    family(B), genus(A);
+    family(B), species(A);
+    genus(B), species(A);
+    hasCompoundName(G, S, A), hasParent(G, B);
+    hasCompoundName(G, S, A), hasParent(S, B).
 
-hasCommonName(N,C) :-
-    commonName(N,_) = C.
+hasCommonName(N, C) :-
+    commonName(N, C);
+    hasCompoundName(G, S, N), commonName(S, C).
 
-hasCommonName(G,S,C) :-
-    commonName(G,S) = C.
+hasCommonName(G, S, C) :-
+    hasParent(S, G), commonName(S, B), B = C.
 
-%hasSciName(?C,?N).
-
-compoundName(G, S) :-
-    genus(G),
-    species(S),
-    append(G, "_", buffer),
-    append(buffer, S, buffer),
-    buffer.
+hasSciName(C, N) :-
+    order(N), hasCommonName(N, C);
+    family(N), hasCommonName(N, C);
+    genus(N), hasCommonName(N, C);
+    hasCompoundName(G, S, N), hasCommonName(S, C).
 
 hasCompoundName(G, S, N) :-
-    compoundName(G, S) = N.
+    compoundName(G, S, N).
 
 %isaStrict(?A, ?B).
 
 %isa(?A, ?B).
 
-%synonym(?A, ?B).
+synonym(A,B) :-
+    hasCommonName(B, A);
+    hasCommonName(A, B).
+    
 
 %countSpecies(?A, -N).
 
